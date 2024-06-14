dc8b6da754484eacf0cc343fb004612dcb71ebaf
congcong919 <congcong.zhao0919@gmail.com>
2024-06-14 16:04:14 +1000

    Sponsor page layout version 2.0

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -8,68 +8,34 @@ import {Container, Row, Col, Card, Button, Image, ListGroup} from 'react-bootstr
 // Later we can get these from cloud, use these examples crawled for now
 const events = [
     {
-        image: "https://placehold.co/300x200",
-        title: "Small dog meetup to celebrate Frankie's birthday",
-        host: "Small Dogs Meetup Group",
-        date: "SUN, JUNE 16 - 09:00 AEST",
-        attendees: "6",
-        cost: "Free"
+        image: "https://placehold.co/300x200"
     },
     {
-        image: "https://placehold.co/300x200",
-        title: "The Future of Work: Navigating the Impact of Generative AI",
-        host: "Responsible AI Melbourne",
-        date: "MON, JUNE 17 - 17:30 AEST",
-        attendees: "121",
-        cost: "Free"
+        image: "https://placehold.co/300x200"
     },
     {
-        image: "https://placehold.co/300x200",
-        title: "Let's Get Together for Breakfast/Coffee and Plan/Embark On Our Next Adventure!!!",
-        host: "Explore Victoria - Weekend Adventures in Regional...",
-        date: "SAT, JUNE 8 - 08:00 AEST",
-        attendees: "21",
-        cost: "Free"
+        image: "https://placehold.co/300x200"
     },
     {
-        image: "https://placehold.co/300x200",
-        title: "Join Us for the Ultimate Culture Exchange Party in Collingwood!",
-        host: "Spectrum",
-        date: "SAT, JUNE 8 - 18:00 AEST",
-        attendees: "93",
-        cost: "Free"
+        image: "https://placehold.co/300x200"
     },
     {
-        image: "https://placehold.co/300x200",
-        title: "v28_Free-Mahi-Mahi!",
-        host: "DesignOps LOL",
-        date: "TUE, JUNE 18 - 18:00 AEST",
-        attendees: "86",
-        cost: "Free"
+        image: "https://placehold.co/300x200"
     },
     {
-        image: "https://placehold.co/300x200",
-        title: "Art Open Space",
-        host: "Melbourne Art and Social Space",
-        date: "SUN, JUNE 9 - 14:00 AEST",
-        attendees: "6",
-        cost: "Free"
+        image: "https://placehold.co/300x200"
+    }
+];
+
+const videos = [
+    {
+        image: "https://placehold.co/1600x900",
     },
     {
-        image: "https://placehold.co/300x200",
-        title: "Join Us for the First-Ever Decision Intelligence Meetup in Australia!",
-        host: "Decision Intelligence",
-        date: "WED, JUNE 26 - 18:00 AEST",
-        attendees: "29",
-        cost: "Free"
+        image: "https://placehold.co/1600x900",
     },
     {
-        image: "https://placehold.co/300x200",
-        title: "Lunch at Rocco's Burger Cafe",
-        host: "Eastside Lesbian Lounge Events (ELLE) 30+",
-        date: "SAT, JUNE 8 - 12:00 AEST",
-        attendees: "19",
-        cost: "Free"
+        image: "https://placehold.co/1600x900",
     }
 ];
 
@@ -78,17 +44,16 @@ function EventCard({ image, title, host, date, attendees, cost }) {
         <Col md={4} className="mb-4">
             <Card>
                 <Card.Img variant="top" src={image} />
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>Hosted by: {host}</Card.Text>
-                    <Card.Text>
-                        <i class="bi bi-calendar-event"></i> {date}
-                    </Card.Text>
-                    <Card.Text>
-                        <i class="bi bi-people"></i> {attendees} going <br />
-                        <i class="bi bi-ticket-perforated"></i> {cost}
-                    </Card.Text>
-                </Card.Body>
+            </Card>
+        </Col>
+    );
+};
+
+function VideoCard({ image, title, host, date, attendees, cost }) {
+    return (
+        <Col md={10} className="mb-4">
+            <Card>
+                <Card.Img variant="top" src={image} />
             </Card>
         </Col>
     );
@@ -104,33 +69,91 @@ export default function Sponsors() {
             <br />
             <Container fluid>
                 <Row>
-                    <Col className="d-flex justify-content-center">
-                        <ListGroup defaultActiveKey="#link1" className="index-content">
-                            <ListGroup.Item action href="#link1">
-                                Introdcution
-                            </ListGroup.Item>
-                            <ListGroup.Item action href="#link2">
-                                Our goal
-                            </ListGroup.Item>
-                            <ListGroup.Item action href="#link3">
-                                Our sponsors and partners 
-                            </ListGroup.Item>
-                            <ListGroup.Item action href="#link4">
-                                Content 1 
-                            </ListGroup.Item>
-                            <ListGroup.Item action href="#link5">
-                                Content 2 
-                            </ListGroup.Item>
-                        </ListGroup>
+                    <Col>
+                        <Container className="index-content">
+                            <h2>
+                                Index of Content
+                            </h2>
+                            <Row>
+                                <div className="index-item">
+                                    <h4><a href="#About">About</a></h4>
+                                    <hr></hr>
+                                </div>
+                            </Row>
+                            <Row>
+                                <div className="index-item">
+                                    <h4><a href="#Sponsors&partners">Sponsors & partners</a></h4>
+                                    <hr></hr>
+                                </div>
+                            </Row>
+                            <Row>
+                                <div className="index-item">
+                                    <h4><a href="#past-events">Past Events</a></h4>
+                                    <hr></hr>
+                                </div>
+                            </Row>
+                            <Row>
+                                <div className="index-item">
+                                    <h4><a href="#contact-us">Contact Us</a></h4>
+                                    <hr></hr>
+                                </div>
+                            </Row>
+                        </Container>
                     </Col>
-                    <Col xs={8}>
-                        <h4 className="my-4">
-                        Our Sponsors and Partners play a crucial rule in the MeetU-friends making platform. Together we accomplish a successful tournament! 
-                        </h4>
+                    <Col xs={8} className="content-section">
+                        <Row id="About">
+                            <h2 className="my-5">
+                                About
+                            </h2>
+                            <h4>
+                                Our Sponsors and Partners play a crucial rule in the MeetU-friends making platform. Together we accomplish a successful tournament! 
+                            </h4>
+                            <br></br>
+                            <br></br>
+                            <br></br>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </Row>
+                        
+                        <Row id="Sponsors&partners">
+                            <h2 className="my-5">
+                                Sponsors & partners
+                            </h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </Row>
                         <Row>
                             {events.map((event, index) => (
                                 <EventCard key={index} {...event} />
                             ))}
+                        </Row>
+                        <Row id="past-events">
+                            <h2 className="my-5">
+                                Past Events
+                            </h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </Row>
+                        <Row className="d-flex justify-content-center">
+                            {videos.map((video, index) => (
+                                <VideoCard key={index} {...video} />
+                            ))}
+                        </Row>
+                        <Row>
+                            {events.map((event, index) => (
+                                <EventCard key={index} {...event} />
+                            ))}
+                        </Row>
+                        <Row id="contact-us">
+                            <h2 className="my-5">
+                                Contact Us
+                            </h2>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
                         </Row>
                     </Col>
                 </Row>
